c263a8fd2f4969ce6ef6c4efea1ad77a5353ae67
Julien Royer <eldebaran@gmail.com>
2019-12-12 16:10:26 +0100

    first commit

---
+++ src/main/java/fr/eimonku/MyBenchmark.java
@@ -0,0 +1,38 @@
+package fr.eimonku;
+
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.State;
+
+//https://blog.soat.fr/2015/07/benchmark-java-introduction-a-jmh/
+//https://stackoverflow.com/questions/504103/how-do-i-write-a-correct-micro-benchmark-in-java
+public class MyBenchmark {
+	@State(Benchmark)
+	public static class UsingVolatile {
+		volatile String name;
+
+		String getName() {
+			return name;
+		}
+	}
+
+	public @Benchmark String usingVolatile(final UsingVolatile o) {
+		return o.getName();
+	}
+
+	@State(Benchmark)
+	public static class MixingVolatileAndNonVolatile {
+		String nonVolatileName;
+		volatile String volatileName;
+
+		String getName() {
+			final String name = nonVolatileName;
+			return name != null ? name : volatileName;
+		}
+	}
+
+	public @Benchmark String mixingVolatileAndNonVolatile(final MixingVolatileAndNonVolatile o) {
+		return o.getName();
+	}
+}
